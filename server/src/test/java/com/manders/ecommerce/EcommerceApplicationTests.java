f34a3f3f09039e15c12a7c5db54332a498894b1c
Manders-Ma <ma880803@gmail.com>
2024-03-01 18:35:56 +0800

    (server) create spring boot project

---
+++ server/src/test/java/com/manders/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manders.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
