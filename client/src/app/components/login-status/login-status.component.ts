5426b88e100a98984480f9975993e0c9effbdffa
Manders-Ma <ma880803@gmail.com>
2024-03-19 14:24:55 +0800

    (client) fix: 修正logout沒有清掉XSRF-TOKEN這個cookie

--- client/src/app/components/login-status/login-status.component.ts
+++ client/src/app/components/login-status/login-status.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { Member } from '../../common/member';
+import { removeCookie } from 'typescript-cookie';
 
 @Component({
   selector: 'app-login-status',
@@ -21,6 +22,10 @@ export class LoginStatusComponent implements OnInit {
   logout() {
     this.loginService.resetAuthenticationState();
     this.loginService.member = new Member();
-    this.storage.setItem("XSRF-TOKEN", "");
+    // 登入登出都做過一次之後，再登入如果沒清掉XSRF-TOKEN，spring會認為你有所以不會給你，
+    // 由於我目前只在login的時候才獲取cookie並儲存到session stotage，所以這次沒拿到會
+    // 導致之後沒有辦法下訂單。
+    removeCookie("XSRF-TOKEN");
+    this.storage.clear();
   }
 }
