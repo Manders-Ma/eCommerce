7efe95f9f3b0b3af21f11b538ee52ff48eb0722e
Manders-Ma <ma880803@gmail.com>
2024-03-29 16:43:44 +0800

    (server) change: order history 用 創建日期做降序排列

--- server/src/main/java/com/manders/ecommerce/dao/OrderRepository.java
+++ server/src/main/java/com/manders/ecommerce/dao/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.manders.ecommerce.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +10,7 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
   
   @Query(value = "select o from Order o where o.orderTrackingNumber = :orderTrackingNumber")
   Order findByOrderTrackingNumber(@Param("orderTrackingNumber") String orderTrackingNumber);
+  
+  @Query(value = "select o from Order o where o.customer.member.email=:email order by o.dateCreated desc")
+  List<Order> findByMemberEmailOrderByDateDesc(@Param("email") String email);
 }
--- server/src/main/java/com/manders/ecommerce/service/OrderHistoryServiceImpl.java
+++ server/src/main/java/com/manders/ecommerce/service/OrderHistoryServiceImpl.java
@@ -1,31 +1,22 @@
 package com.manders.ecommerce.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.manders.ecommerce.dao.MemberRepository;
-import com.manders.ecommerce.entity.Customer;
-import com.manders.ecommerce.entity.Member;
+import com.manders.ecommerce.dao.OrderRepository;
 import com.manders.ecommerce.entity.Order;
 
 @Service
 public class OrderHistoryServiceImpl implements OrderHistoryService {
   
   @Autowired
-  private MemberRepository memberRepository;
+  private OrderRepository orderRepository;
 
   @Override
   @Transactional
   public List<Order> getOrderHistory(String email) {
-    
-    Member member = memberRepository.findByEmail(email);
-    List<Order> orderHistories = new ArrayList<>();
-    
-    for (Customer customer: member.getCustomers()) {
-      customer.getOrders().forEach(order -> orderHistories.add(order));
-    }
+    List<Order> orderHistories = orderRepository.findByMemberEmailOrderByDateDesc(email);
     
     return orderHistories;
   }
