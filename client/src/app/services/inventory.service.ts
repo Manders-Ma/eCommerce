c2aed643fc42d70f6422cf53b850494a30a8e05b
Manders-Ma <ma880803@gmail.com>
2024-04-11 15:36:58 +0800

    (client) refactor: 調整api名字讓他符合restful api命名風格

--- client/src/app/services/inventory.service.ts
+++ client/src/app/services/inventory.service.ts
@@ -15,17 +15,14 @@ export class InventoryService {
   constructor(private httpClient: HttpClient) { }
 
   deleteProductById(theProductId: number): Observable<any> {
-    const deleteUrl = `${this.inventoryUrl}/delete-product/${theProductId}`
-    return this.httpClient.delete(deleteUrl, { observe: 'response', withCredentials: true });
+    return this.httpClient.delete(this.inventoryUrl + `/${theProductId}`, { observe: 'response', withCredentials: true });
   }
 
   updateProduct(theProduct: Product): Observable<any> {
-    const updateUrl = `${this.inventoryUrl}/update-product`;
-    return this.httpClient.patch(updateUrl, theProduct, { observe: 'response', withCredentials: true });
+    return this.httpClient.patch(this.inventoryUrl, theProduct, { observe: 'response', withCredentials: true });
   }
 
   saveProduct(productCreation: ProductCreation): Observable<any> {
-    const createUrl = `${this.inventoryUrl}/create-product`;
-    return this.httpClient.post(createUrl, productCreation, { observe: 'response', withCredentials: true });
+    return this.httpClient.post(this.inventoryUrl, productCreation, { observe: 'response', withCredentials: true });
   }
 }
